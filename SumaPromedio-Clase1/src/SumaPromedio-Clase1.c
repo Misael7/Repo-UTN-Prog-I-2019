17c29eeca8759e8c38fb4fbd307a7aba4a82b8c6
Misael7 <misael.ec7@gmail.com>
2019-08-25 13:55:41 -0300

    Suma de promedios con punteros

--- SumaPromedio-Clase1/src/SumaPromedio-Clase1.c
+++ SumaPromedio-Clase1/src/SumaPromedio-Clase1.c
@@ -1,7 +1,7 @@
 /*
  ============================================================================
  Name        : SumaPromedio-Clase1.c
- Author      : 
+ Author      : Misael Emmanuel Cortes
  Version     :
  Copyright   : Your copyright notice
  Description : Hello World in C, Ansi-style
@@ -10,8 +10,28 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+int hacerSumas (int *resultado);
 
-int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+int main(void)
+	{
+	int resultado;
+	hacerSumas(&resultado);
+	printf("El promedio es:%d",resultado);
+	return 0;
+	}
+
+int hacerSumas (int *resultado)
+	{
+	int numIngreso;
+	int cuentaIngresos=0;
+	int sumaIngresos=0;
+	while(cuentaIngresos<5)
+		{
+			printf("Ingrese 5 num");
+			scanf("%d",&numIngreso);
+			sumaIngresos=numIngreso+sumaIngresos;
+			cuentaIngresos++;
+		}
+	*resultado=sumaIngresos/cuentaIngresos;
+	return 0;
 }
