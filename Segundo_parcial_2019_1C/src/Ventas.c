3f06aeee9210dda8d3be974ee908b24022215636
alumno <alumno@LAB00PC00>
2019-11-20 20:58:22 -0300

    ...

--- Segundo_parcial_2019_1C/src/Ventas.c
+++ Segundo_parcial_2019_1C/src/Ventas.c
@@ -6,3 +6,34 @@
  */
 
 #include "Ventas.h"
+
+void venta_delete()
+{
+	void free(void*);
+}
+
+Ventas* venta_new()
+{
+	return malloc(sizeof(Ventas));
+}
+
+Ventas* venta_newParametros(char* id_venta,char* fecha_venta,char* tipo_foto,char* cantidad,char* precio_unitario, char*cuit_cliente)
+{
+	Ventas* retorno = NULL;
+	Ventas *this;
+
+	return retorno;
+}
+
+int venta_setID(Ventas* this,char* id_venta)
+{
+	int retorno = EXIT_FAILURE;
+
+	if(this != NULL)
+	{
+		strcpy(this->id_venta,id_venta);
+		retorno = EXIT_SUCCESS;
+	}
+
+	return retorno;
+}
--- Segundo_parcial_2019_1C/src/Ventas.h
+++ Segundo_parcial_2019_1C/src/Ventas.h
@@ -7,6 +7,11 @@
 
 #ifndef VENTAS_H_
 #define VENTAS_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio_ext.h>
+#include <string.h>
+#include "LinkedList.h"
 
 typedef struct
 {
@@ -17,5 +22,9 @@ typedef struct
 	char precio_unitario[50];
 	char cuit_cliente[50];
 }Ventas;
+
+Ventas* venta_new();
+Ventas* venta_newParametros(char* id_venta,char* fecha_venta,char* tipo_foto,char* cantidad,char* precio_unitario, char*cuit_cliente);
+void venta_delete();
 
 #endif /* VENTAS_H_ */
